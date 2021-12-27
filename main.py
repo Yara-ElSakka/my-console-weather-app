f305e99e05e2ddd6786df19c66638155a9bbc2c7
Yara ElSakka <95961383+Yara-ElSakka@users.noreply.github.com>
2021-12-27 16:34:09 +0400

    providing additional information such as time and location of the city.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # 20.12.21
 # https://planningclasses.notion.site/Class-20-12-2021-b520ff3d833d4db383bd9b311d35be90
 # https://codefile.io/f/4BfjySsT3Ai05dygyUyh
+# added other information API on 27.12.21
+#
 
 
 from requests import *
@@ -13,11 +15,29 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 def getWeatherMethod(cityTemp):
     tempDB = get(f'https://www.google.com/search?q={cityTemp}&oq={cityTemp}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8',headers=headers)
     soup = BeautifulSoup(tempDB.text, 'html.parser')
+
+    # API to get the weather temparature:
     weatherID = soup.select('#wob_tm')[0].get_text()
     print(weatherID+" C")
-    #return weatherID
 
-cityName = input("City Temperature: ")
+    print("\n..working out other information..\n")
+    # additional information as per 27.12.21 updates:
+
+    # API to get the location information:
+    location = soup.select('#wob_loc')[0].getText()
+    print(f"Location information: {location}\n")
+
+    # API to get the time information:
+    time = soup.select('#wob_dts')[0].getText()
+    print(f"Time information: {time}\n")
+
+    # API to get other info:
+    info = soup.select('#wob_dc')[0].getText()
+    print(f"Other information: {info}\n")
+
+    #return weatherID // "no need for return command as per the teacher"
+
+cityName = input("The city temperature: ")
 cityName = cityName + " weather now is" # weather will be replaced with the actual weather in Celsius
 print(cityName)
 getWeatherMethod(cityName)
